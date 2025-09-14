488d621de068500069e0db7c274712b2c2bf1b04
Ralchuu <rauli.saresoja2@gmail.com>
2025-09-14 14:49:15 +0300

    first commit

---
+++ src/main/java/hh/bookstore/rauli/RauliApplication.java
@@ -0,0 +1,13 @@
+package hh.bookstore.rauli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RauliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RauliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/bookstore/rauli/RauliApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.bookstore.rauli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RauliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
